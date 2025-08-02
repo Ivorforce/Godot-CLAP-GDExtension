f6ca7c8154b94130d15300ede6f006ed8860c130
Lukas Tenbrink <lukas.tenbrink@gmail.com>
2025-08-02 18:44:27 +0200

    Add CLAP headers.

--- src/example_class.cpp
+++ src/example_class.cpp
@@ -1,9 +1,13 @@
 #include "example_class.h"
 
+#include "clap/clap.h"
+
 void ExampleClass::_bind_methods() {
 	godot::ClassDB::bind_method(D_METHOD("print_type", "variant"), &ExampleClass::print_type);
 }
 
 void ExampleClass::print_type(const Variant &p_variant) const {
-	print_line(vformat("Type: %d", p_variant.get_type()));
+	clap_plugin_entry entry;
+	entry.clap_version = CLAP_VERSION;
+	print_line(vformat("CLAP version: %d.%d.%d", entry.clap_version.major, entry.clap_version.minor, entry.clap_version.revision));
 }
